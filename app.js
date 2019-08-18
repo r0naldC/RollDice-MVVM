4d6d72a6918e1658733373b0cbef789479908853
¨David¨ <davidrr261096@hotmail.com>
2019-08-17 21:54:12 -0400

    expert level complete

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function Question(question, answer, correctAnswer) {
 
 var listOfQuestions = [
     new Question("is this challenge fun?", ["no", "realmente no se", "si"], "si"),
-    new Question("what day is today?", ["domingo", "viernes", "martes"], "viernes"),
+    new Question("what day is today?", ["domingo", "sabado", "martes"], "sabado"),
     new Question("how old is O&M?", [53,25,48], 53)
 ];
 
@@ -17,8 +17,12 @@ function SelectRandomQuestion() {
     return question;
 }
 
-function PrintQuestionOut(randomQuestion) {
-    var question = randomQuestion.question
+function PrintQuestionOut() {
+    var randomQuestion = SelectRandomQuestion();
+
+    if(listOfQuestions.length === 0)return console.log("There's not more question, Game Ends");
+    
+    var question = randomQuestion.question;
 
     console.log("\n");
     console.log(question);
@@ -26,28 +30,39 @@ function PrintQuestionOut(randomQuestion) {
     for (var i = 0; i < randomQuestion.answer.length; i++) {
         console.log(i + "- " + randomQuestion.answer[i]);
     }
+
+    userAnswer(randomQuestion);
 }
 
-function UserAnswer(randomQuestion) {
-    var userAnswer = prompt("Answer the question printed in the console (use the number to answer)");
+function UserAnswer() {
     var userScore = 0;
 
-    return function() {
-        var answer = randomQuestion.answer[userAnswer];
-        if(answer === randomQuestion.correctAnswer)
+    return function(randomQuestion) {
+        var userAnswer = prompt("Answer the question printed in the console (use the number to answer)");
+        if(userAnswer !== "exit")
         {
-            userScore += 1; 
-            console.log("The answer is correct, you earn 1 point");
-            console.log("Your score is " + userScore);
+            var answer = randomQuestion.answer[userAnswer];
+            if(answer === randomQuestion.correctAnswer)
+            {
+                userScore += 1; 
+                console.log("The answer is correct, you earn 1 point");
+                console.log("Your score is " + userScore);
+                listOfQuestions.splice(listOfQuestions.indexOf(randomQuestion),1);
+
+            }else{
+                console.log("The answer is wrong");
+            }
+            NextQuestion();
 
         }else{
-            console.log("The answer is wrong");
+            console.log("Game Ends");
         }
-
     }
 }
 
-var randomQuestion = SelectRandomQuestion();
-PrintQuestionOut(randomQuestion);
-UserAnswer(randomQuestion)();
+function NextQuestion() {
+    PrintQuestionOut();
+}
 
+var userAnswer = UserAnswer();
+NextQuestion();
